71c32ebb3156a6fb9ac8c59581ed7eed264f2001
Akshay Gadegone <akshay.gadegone@gmail.com>
2017-08-06 18:05:31 -0700

    Making it functional[C

--- main.js
+++ main.js
@@ -1,7 +1,25 @@
  $(document).ready(function(){
    $('#search').click(function(){
-     var searchText = $('#searchText').val();
-     var api = "https://en.wikipedia.org/w/api.php?action=opensearch&search="+searchText+"&limit=5";
+     var searchText = $('#searchBar').val();
+     var limit=5;
+     var api = "https://en.wikipedia.org/w/api.php?action=opensearch&search="+searchText+"&limit="+limit;
      console.log(api);
+
+     $.ajax({
+       type:"GET",
+       url:api,
+       async:false,
+       dataType:"json",
+       success:function(data){
+         for(var i=0;i<limit;i++){
+           $('.output'+i).addClass("btn btn-default").append("<a href="+data[3][i]+">"+data[1][i]+"</a><p>"+data[2][i]+"</p>");
+           //$('.output').append("<br>")
+         }
+       },
+       error:function() {
+
+       }
+     });
+
    });
  });
